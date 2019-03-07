1e3e35be92624701111ee7e6ad17634bbc9a9b7e
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2019-03-07 10:14:43 +0100

    Move backbones to backbones module

--- resnet-stardist.py
+++ resnet-stardist.py
@@ -4,17 +4,17 @@ from keras import layers
 from keras import models
 import keras.backend as K
 
-from utils.tensorboard import write_graph_to_tensorboard
-from resnext import resnext_101
+from keras_transfer_learning.utils.tensorboard import write_graph_to_tensorboard
+from keras_transfer_learning.backbones.resnet import resnet_50
 
 # Build the model
 inp = layers.Input(shape=(224, 224, 3))
-oup = resnext_101(inp)
+oup = resnet_50(inp)
 
 m = models.Model(inp, oup)
 
 # Try to load the weights
-m.load_weights('/home/bw/Downloads/resnext101_weights_tf_dim_ordering_tf_kernels.h5', by_name=True)
+# m.load_weights('/home/bw/Downloads/resnext101_weights_tf_dim_ordering_tf_kernels.h5', by_name=True)
 
 # Print a summary and write to tensorboard
 m.summary()
